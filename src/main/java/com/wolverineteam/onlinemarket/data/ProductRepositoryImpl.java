01b070b75512694a89ed6ef079502cb77d4a44b8
Krasimir Zahariev <k.zahariew@abv.bg>
2018-07-28 23:45:27 +0300

    add some functionality to the products repo

--- src/main/java/com/wolverineteam/onlinemarket/data/ProductRepositoryImpl.java
+++ src/main/java/com/wolverineteam/onlinemarket/data/ProductRepositoryImpl.java
@@ -2,21 +2,49 @@ package com.wolverineteam.onlinemarket.data;
 
 import com.wolverineteam.onlinemarket.data.base.ProductRepository;
 import com.wolverineteam.onlinemarket.models.Product;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
 
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public ProductRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public Product getById(int id) {
-        return null;
+        Product product = null;
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            product = session.get(Product.class, id);
+            session.getTransaction().commit();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return product;
     }
 
     @Override
     public List<Product> getAll() {
-        return null;
+        List<Product> products = new ArrayList<>();
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            products = session.createQuery("from Product", Product.class).list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return products;
     }
 
     @Override
